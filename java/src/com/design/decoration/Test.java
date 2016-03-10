f537d2acc166468572e8f0f559cb40734c721831
wayne80 <wayne80@users.noreply.github.com>
2016-03-10 15:59:43 +0800

    init decoration

---
+++ java/src/com/design/decoration/Test.java
@@ -0,0 +1,11 @@
+package com.design.decoration;
+
+/**
+ * Created by 461246 on 2016/3/10.
+ */
+public class Test {
+
+    public static void  main(String[] args) {
+        System.out.print("hello world");
+    }
+}
